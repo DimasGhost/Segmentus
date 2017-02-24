0249732a8a3991433311fde4923e0ccdc2887502
DimasGhost <strokovghost@gmail.com>
2017-02-24 20:05:55 +0300

    Added ColorBank class

---
+++ ColorBank.cs
@@ -0,0 +1,13 @@
+using Android.Graphics;
+
+namespace Segmentus
+{
+    static class ColorBank
+    {
+        static public Color black = new Color(85, 85, 85);
+        static public Color white = new Color(244, 243, 238);
+        static public Color blue = new Color(213, 225, 234);
+        static public Color red = new Color(238, 99, 82);
+        static public Color yellow = new Color(255, 209, 81);
+    }
+}
