0ab52a4b913cbfed202f1a7253fa68abdd78a498
DimasGhost <strokovghost@gmail.com>
2017-04-15 21:46:04 +0300

    Added multi screen resolution support

--- GameView.cs
+++ GameView.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Android.Content;
 using Android.Graphics;
 using Android.Util;
@@ -13,14 +15,27 @@ namespace Segmentus
         public delegate void DrawDelegate(Canvas canvas);
         public static event DrawDelegate DrawEvent;
 
+        public const int canonWidth = 720;
+        public const int canonHeight = 1280;
+
+        public static int xCenter, yCenter;
+        public static float scaleFactor;
+
         public GameView(Context context, IAttributeSet attrs) : base(context, attrs, 0)
         {
             Instance = this;
+
+            xCenter = this.Width / 2;
+            yCenter = this.Height / 2;
+            scaleFactor = Math.Min(this.Width / canonWidth, this.Height / canonHeight);
         }
 
         protected override void OnDraw(Canvas canvas)
         {
+            canvas.Save();
+            canvas.Translate(xCenter, yCenter);
             DrawEvent?.Invoke(canvas);
+            canvas.Restore();
         }
     }
 }
