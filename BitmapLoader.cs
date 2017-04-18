45ba904735e1b6d3fd3b5ecc5b69e1bef15faca8
DimasGhost <strokovghost@gmail.com>
2017-04-18 18:30:26 +0300

    Improved of bitmap loading

--- BitmapLoader.cs
+++ BitmapLoader.cs
@@ -1,13 +1,24 @@
+using System;
 using Android.App;
+using Android.Content.Res;
 using Android.Graphics;
 
 namespace Segmentus
 {
     static class BitmapLoader
     {
-        public static Bitmap LoadAndResize(int res_id, int width, int height)
+        public static Bitmap LoadAndResize(int resID, int width, int height)
         {
-            Bitmap source = BitmapFactory.DecodeResource(Application.Context.Resources, res_id);
+            Resources res = Application.Context.Resources;
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.InJustDecodeBounds = true;
+            BitmapFactory.DecodeResource(res, resID, options);
+            int sourceWidth = options.OutWidth;
+            int sourceHeight = options.OutHeight;
+            if (sourceWidth > width && sourceHeight > height)
+                options.InSampleSize = Math.Min(sourceWidth / width, sourceHeight / height);
+            options.InJustDecodeBounds = false;
+            Bitmap source = BitmapFactory.DecodeResource(res, resID, options);
             Bitmap scaled = Bitmap.CreateScaledBitmap(source, width, height, true);
             return scaled;
         }
