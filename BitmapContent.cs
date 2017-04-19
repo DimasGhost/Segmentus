08f3c478ebfadff0af456072d1023f9d3d363b1e
DimasGhost <strokovghost@gmail.com>
2017-04-19 18:12:20 +0300

    Precounting of lefttop of BitmapContent

--- BitmapContent.cs
+++ BitmapContent.cs
@@ -4,17 +4,24 @@ namespace Segmentus
 {
     class BitmapContent : DrawablePart
     {
+        int xLeft, yTop;
         public Bitmap bitmap;
         public BitmapContent(Bitmap bitmap, Pivot parentPivot, float x = 0, float y = 0) :
             base(parentPivot, x, y)
         {
             this.bitmap = bitmap;
+            RecountLeftTop();
+            pivot.Changed += RecountLeftTop;
+        }
+
+        void RecountLeftTop()
+        {
+            xLeft = (int)(pivot.X - bitmap.Width / 2);
+            yTop = (int)(pivot.Y - bitmap.Height / 2);
         }
 
         protected override void Draw(Canvas canvas)
         {
-            float xLeft = pivot.X - bitmap.Width / 2;
-            float yTop = pivot.Y - bitmap.Height / 2;
             canvas.DrawBitmap(bitmap, xLeft, yTop, null);
         }
     }
