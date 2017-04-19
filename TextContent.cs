0216f395003223449d47de51e4c4e4586e418e13
DimasGhost <strokovghost@gmail.com>
2017-04-19 21:37:56 +0300

    Added TextContent class

---
+++ TextContent.cs
@@ -0,0 +1,49 @@
+using Android.App;
+using Android.Graphics;
+
+namespace Segmentus
+{
+    class TextContent : DrawablePart
+    {
+        string text;
+        int colorID;
+        float size;
+        int originX, originY;
+
+        static protected Paint paint;
+        static TextContent()
+        {
+            Typeface tf = Typeface.CreateFromAsset(Application.Context.Assets, 
+                "fonts/manteka.ttf");
+            paint = new Paint();
+            paint.SetTypeface(tf);
+            paint.TextAlign = Paint.Align.Left;
+        }
+
+        public TextContent(string text, int colorID, float size,
+            Pivot parentPivot, float x = 0, float y = 0): base(parentPivot, x, y)
+        {
+            this.text = text;
+            this.colorID = colorID;
+            this.size = size;
+            RecountOrigin();
+            pivot.Changed += RecountOrigin;
+        }
+
+        void RecountOrigin()
+        {
+            Rect r = new Rect();
+            paint.TextSize = size;
+            paint.GetTextBounds(text, 0, text.Length, r);
+            originX = (int)(pivot.X - r.Width() / 2 - r.Left / 2);
+            originY = (int)(pivot.Y + r.Height() / 2 - r.Bottom / 2);
+        }
+
+        protected override void Draw(Canvas canvas)
+        {
+            paint.Color = ColorBank.GetColor(colorID);
+            paint.TextSize = size;
+            canvas.DrawText(text, originX, originY, paint);
+        }
+    }
+}
