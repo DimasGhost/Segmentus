603d4525fb6aba1c9e560a41afe241a5d333408d
DimasGhost <strokovghost@gmail.com>
2017-04-23 23:15:27 +0300

    Added localBounds and call of RecountBounds method to TouchablePart

--- TouchablePart.cs
+++ TouchablePart.cs
@@ -5,15 +5,24 @@ namespace Segmentus
     abstract class TouchablePart : DrawablePart
     {
         public Rect bounds;
-        public TouchablePart(Pivot parentPivot, float x = 0, float y = 0)
-            : base(parentPivot, x, y) { }
+        Rect localBounds;
+        public TouchablePart(Rect localBounds, Pivot parentPivot, float x = 0, float y = 0)
+            : base(parentPivot, x, y) {
+            this.localBounds = localBounds;
+        }
 
         public virtual void OnTouchDown(int x, int y) { }
         public virtual void OnTouchUp(int x, int y) { }
         public virtual void OnTouchMove(int x, int y) { }
         public virtual void OnTouchOutside(int x, int y) { }
         public virtual void OnTouchCancel(int x, int y) { }
-        protected abstract void RecountBounds();
+        protected void RecountBounds()
+        {
+            bounds.Left = (int)pivot.AbsX + localBounds.Left;
+            bounds.Right = (int)pivot.AbsX + localBounds.Right;
+            bounds.Top = (int)pivot.AbsY + localBounds.Top;
+            bounds.Bottom = (int)pivot.AbsY + localBounds.Bottom;
+        }
 
         public virtual void Activate()
         {
