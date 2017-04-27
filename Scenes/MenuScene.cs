618d9e64a54635d188d7e0a2ababfeefd5122a3d
DimasGhost <strokovghost@gmail.com>
2017-04-28 02:12:18 +0300

    Added buttons to Menu scene

--- Scenes/MenuScene.cs
+++ Scenes/MenuScene.cs
@@ -18,8 +18,8 @@ namespace Segmentus.Scenes
     {
         public static MenuScene Instance { get; set; }
 
-        TextContent titleText;
-        TextContent authorText;
+        TextContent titleText, authorText;
+        Button playButton, rankButton, profileButton, helpButton;
 
         public MenuScene() : base()
         {
@@ -29,14 +29,47 @@ namespace Segmentus.Scenes
             authorText = new TextContent("BY MAUNT", ColorBank.Yellow,
                 28 * GameView.scaleFactor, pivot, 190 * GameView.scaleFactor,
                 -350 * GameView.scaleFactor);
+
+            playButton = CreateHexagonButton((int)(320 * GameView.scaleFactor),
+                Resource.Drawable.menu_play, 0, -20 * GameView.scaleFactor);
+
+            rankButton = CreateHexagonButton((int)(190 * GameView.scaleFactor),
+                Resource.Drawable.menu_leaderboards, -160 * GameView.scaleFactor, 
+                225 * GameView.scaleFactor);
+
+            profileButton = CreateHexagonButton((int)(190 * GameView.scaleFactor),
+                Resource.Drawable.menu_profile, 160 * GameView.scaleFactor,
+                225 * GameView.scaleFactor);
+
+            helpButton = CreateHexagonButton((int)(190 * GameView.scaleFactor),
+                Resource.Drawable.menu_help, 0, 410 * GameView.scaleFactor);
         }
 
-        protected override void OnShow() { }
+        Button CreateHexagonButton(int size, int id, float x, float y)
+        {
+            int hsize = (int)(0.85 * size);
+            int vsize = (int)(0.9 * size);
+            Rect bounds = new Rect(-hsize / 2, -vsize / 2, hsize / 2, vsize / 2);
+            Bitmap bitmap = BitmapLoader.LoadAndResize(id, size, size);
+            BitmapContent content = new BitmapContent(bitmap, null);
+            return new Button(content, bounds, pivot, x, y);
+        }
+
+        protected override void OnShow() {
+            playButton.Activate();
+            rankButton.Activate();
+            profileButton.Activate();
+            helpButton.Activate();
+        }
 
         protected override void Draw(Canvas canvas)
         {
             titleText.OnDraw(canvas);
             authorText.OnDraw(canvas);
+            playButton.OnDraw(canvas);
+            rankButton.OnDraw(canvas);
+            profileButton.OnDraw(canvas);
+            helpButton.OnDraw(canvas);
         }
     }
 }
