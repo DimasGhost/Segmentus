36e745ca20638bb36305499f0c5e2d9266c484c8
DimasGhost <strokovghost@gmail.com>
2017-04-30 21:11:40 +0300

    Fixed debug draw for button

--- Button.cs
+++ Button.cs
@@ -72,6 +72,9 @@ namespace Segmentus
         protected override void Draw(Canvas canvas)
         {
             canvas.Save();
+            canvas.Scale(currentDiveScale, currentDiveScale);
+            face.OnDraw(canvas);
+            canvas.Restore();
             if (DebugDraw)
             {
                 Paint p = new Paint();
@@ -79,9 +82,6 @@ namespace Segmentus
                 p.SetStyle(Paint.Style.Stroke);
                 canvas.DrawRect(localBounds, p);
             }
-            canvas.Scale(currentDiveScale, currentDiveScale);
-            face.OnDraw(canvas);
-            canvas.Restore();
         }
 
     }
