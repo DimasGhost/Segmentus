34341138bf2c8dafebdba3cb427c5bf34538f637
DimasGhost <strokovghost@gmail.com>
2017-05-16 23:31:05 +0300

    Added RectContent class

---
+++ RectContent.cs
@@ -0,0 +1,27 @@
+using Android.Graphics;
+
+namespace Segmentus
+{
+    class RectContent : DrawablePart
+    {
+        static Paint paint = new Paint(PaintFlags.AntiAlias);
+        Rect rect;
+
+        static RectContent()
+        {
+            paint.Color = ColorBank.GetColor(ColorBank.Red);
+            paint.StrokeWidth = 6 * GameView.scaleFactor;
+        }
+
+        public RectContent(Rect rect, Pivot parentPivot = null,
+            float x = 0, float y = 0) : base(parentPivot, x, y)
+        {
+            this.rect = rect;
+        }
+
+        protected override void Draw(Canvas canvas)
+        {
+            canvas.DrawRect(rect, paint);
+        }
+    }
+}
